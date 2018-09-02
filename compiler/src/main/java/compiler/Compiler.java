d42a77d32d27cd2efa16ae068dffaaf1d15bd86c
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2018-09-02 13:41:36 +1000

    Changed to take the filename as the first argument

--- compiler/src/main/java/compiler/Compiler.java
+++ compiler/src/main/java/compiler/Compiler.java
@@ -11,21 +11,21 @@ import codegen.Codegen;
 import parser.Parser;
 import parser.Program;
 import parser.Parser.Token;
-import parser.Program.Definition;
 
 public class Compiler {
     public static void main(String[] args) {
         List<Token> tokens = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        String filename = in.nextLine();
-        in.close();
+        if (args.length != 1) {
+            throw new RuntimeException("Please pass a filename as the only parameter");
+        }
+        String filename = args[0];
 		try {
 			tokens = Parser.tokenize(filename);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
         Program program = Parser.parse(tokens);
-        
+
         CheckedProgram checked = Checker.check(program);
         try {
             Codegen.gen(checked, new BufferedWriter(new OutputStreamWriter(System.out)));
